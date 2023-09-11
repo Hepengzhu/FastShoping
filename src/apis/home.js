40a40a49aac3ae26643546c108feeb6b6af2b5a8
Hepengzhu <2949430899@qq.com>
2023-09-11 12:34:39 +0800

    新鲜好物+人气推荐实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -7,3 +7,22 @@ export function getBannerApi(){
         url:'/home/banner'
     })
 }
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const getNewAPI = () => {
+    return httpInstance({
+      url:'/home/new'
+    })
+  }
+
+
+// 获取人气推荐
+  export const getHotAPI = ()=>{
+    return httpInstance({
+      url:'/home/hot'
+    })
+  }
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeCategory from './components/HomeCategory.vue';
 import HomeHot from './components/HomeHot.vue';
 import HomeProduct from './components/HomeProduct.vue';
 import HomeNew from './components/HomeNew.vue'
+import HomePanel from './components/HomePanel.vue';
 </script>
 <template>
     <div class="container">
@@ -13,6 +14,8 @@ import HomeNew from './components/HomeNew.vue'
     <HomeNew />
     <HomeHot />
     <HomeProduct />
+    <!-- 测试 组件 -->
+    <HomePanel title="新鲜好物" sub-title="jjjjjjjjj"></HomePanel>
 </template>
 <style scoped>
  
