b0430f06f21c0dd0e86282f22fbf9af8ee33494b
Hepengzhu <2949430899@qq.com>
2023-09-12 21:12:12 +0800

    GoodsItem组件的封装

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -3,7 +3,7 @@ import HomePanel from './HomePanel.vue'
 import {getGoodsAPI} from '@/apis/home.js'
 import { onMounted, ref } from 'vue'
 import { useLazyData } from '@/hooks/index.js'
-
+import GoodsItem from './GoodsItem.vue'
 // 优化
 const {'target':goods,'result':goodsProduct} = useLazyData(getGoodsAPI)
 
@@ -30,12 +30,7 @@ const {'target':goods,'result':goodsProduct} = useLazyData(getGoodsAPI)
         </RouterLink>
         <ul class="goods-list">
           <li v-for="good in cate.goods" :key="good.id">
-            <RouterLink to="/" class="goods-item">
-              <img v-img-lazy="good.picture" alt="" />
-              <p class="name ellipsis">{{ good.name }}</p>
-              <p class="desc ellipsis">{{ good.desc }}</p>
-              <p class="price">&yen;{{ good.price }}</p>
-            </RouterLink>
+            <GoodsItem :good="good"></GoodsItem>
           </li>
         </ul>
       </div>
@@ -131,41 +126,7 @@ const {'target':goods,'result':goodsProduct} = useLazyData(getGoodsAPI)
       }
     }
 
-    .goods-item {
-      display: block;
-      width: 220px;
-      padding: 20px 30px;
-      text-align: center;
-      transition: all .5s;
 
-      &:hover {
-        transform: translate3d(0, -3px, 0);
-        box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
-      }
-
-      img {
-        width: 160px;
-        height: 160px;
-      }
-
-      p {
-        padding-top: 10px;
-      }
-
-      .name {
-        font-size: 16px;
-      }
-
-      .desc {
-        color: #999;
-        height: 29px;
-      }
-
-      .price {
-        color: $priceColor;
-        font-size: 20px;
-      }
-    }
   }
 }
 </style>
