723cbfe4d21ee84fa44993b87a942f1bbfcbf28e
Hepengzhu <2949430899@qq.com>
2023-09-26 15:25:42 +0800

    图片预览-小图切换大图显示

--- src/views/Detail/indeX.vue
+++ src/views/Detail/indeX.vue
@@ -1,5 +1,6 @@
 <script setup>
 import DetailHot from './components/DetailHot.vue';
+import ImagView from '@/components/ImageView/index.vue'
 import {ref,onMounted} from 'vue'
 import {getDetail} from '@/apis/detail.js'
 import {useRoute} from 'vue-router'
@@ -35,7 +36,7 @@ onMounted(() => {
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+              <ImageView></ImageView>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
