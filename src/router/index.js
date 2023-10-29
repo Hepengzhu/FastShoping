92e5caf7c501829e4e4b913d9f119e682209c94e
Hepengzhu <2949430899@qq.com>
2023-10-29 18:38:26 +0800

    打包

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory,createWebHashHistory } from 'vue-router'
 import Layout from '../views/Layout/index.vue'
 import Login from '../views/Login/index.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path:'/',
