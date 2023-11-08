1cbd8f3c338220dcf8dabdc2af2a27cb56425195
Hepengzhu <2949430899@qq.com>
2023-11-08 22:08:18 +0800

    提交

--- src/stores/category.js
+++ src/stores/category.js
@@ -4,11 +4,11 @@ import {getCategoryAPI} from '@/apis/layout.js'
 
 
 export const useCategoryStore = defineStore('category', () => {
-// 菜单导航栏
-const categoryList = ref([])
-const getCategory = async ()=>{
-    const res = await getCategoryAPI()
-    categoryList.value = res.result
-}
-  return { categoryList, getCategory }
+  // 菜单导航栏
+  const categoryList = ref([])
+  const getCategory = async ()=>{
+      const res = await getCategoryAPI()
+      categoryList.value = res.result
+  }
+    return { categoryList, getCategory }
 })
